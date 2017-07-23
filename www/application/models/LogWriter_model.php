f1a6a9367fc62d614a21e64287e070fea82e54ed
WansangHan <sang5513@naver.com>
2017-07-23 22:34:22 +0900

    에러, 경고 등의 내용이 없을 시 return, return값 제거

--- www/application/models/LogWriter_model.php
+++ www/application/models/LogWriter_model.php
@@ -10,15 +10,16 @@ class LogWriter_model extends CI_Model {
 
     public function ErrorWriter()
     {
+        if(!file_get_contents('php://input')) return;
         $input = json_decode(file_get_contents('php://input'), true);
 
         $q1 = "
 			SELECT INSERT_LOG_MESSAGE('".$input['cate']."', '".$input['level']."', '".$input['message']."') FROM DUAL
 		";
         log_message('debug', $q1);
-        $result = $this->db->query($q1);
+        $this->db->query($q1);
 
-        return json_encode($result);
+        return;
     }
 }
 ?>
